5022252a9368539081d4dd3af97d08a6d9d0e624
jack <jackcrumpleys@gmail.com>
2020-10-13 11:51:43 +1300

    update map

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1602141930",
+        "cacheTag": "1602543090",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "world",
-            "last_rendertime": 1602140369,
+            "last_rendertime": 1602542610,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
